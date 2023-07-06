26700d9dad7befb61ca36e05f177c6a131e157f4
MAHossain1 <mahpro110@gmail.com>
2023-07-06 18:44:51 +0600

    middleware use in redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice";
+import logger from "./middleware/logger";
+// import logger from "redux-logger";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
   },
   // devTools: true
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(logger),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
