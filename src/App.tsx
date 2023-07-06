13a315c637223735db1aba00ef62258113f45775
MAHossain1 <mahpro110@gmail.com>
2023-07-06 07:52:20 +0600

    slice connected with store and implement store's functionality with component function done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "./redux/store";
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+
 function App() {
-  return <h1 className="text-3xl bg-red-500">Tech Net</h1>;
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <div className="flex gap-6">
+        <button
+          className="border-2 border-green-500 rounded-md px-2 py-3"
+          onClick={() => dispatch(increment())}
+        >
+          {" "}
+          Increment
+        </button>
+        <button
+          className="border-2 border-green-500 rounded-md px-2 py-3"
+          onClick={() => dispatch(incrementByAmount(5))}
+        >
+          Increment by Value
+        </button>
+        <div>{count}</div>
+        <button
+          className="border-2 border-yellow-500 rounded-md px-2 py-3"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
